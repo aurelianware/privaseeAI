47626d5fdb99a3c7109b93f7b3cac758dfff771a
aurelianware <markus@aurelianware.com>
2026-02-20 22:08:28 -0700

    fix: also clear all browser caches on load to bust stale SW assets

--- src/main.tsx
+++ src/main.tsx
@@ -12,6 +12,9 @@ if ('serviceWorker' in navigator) {
   navigator.serviceWorker.getRegistrations().then(regs => {
     regs.forEach(r => r.unregister());
   });
+  if ('caches' in window) {
+    caches.keys().then(keys => keys.forEach(k => caches.delete(k)));
+  }
 }
 
 const msalInstance = new PublicClientApplication(msalConfig)
